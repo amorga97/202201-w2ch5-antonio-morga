c5ab4fa52f4f6cec24b03cabbc865b4e03a5123a
Antonio Morga <anotniomc9719@gmail.com>
2022-01-29 20:14:10 +0100

    deleted useless test file

--- game-of-life.js
+++ game-of-life.js
@@ -1,18 +1,8 @@
-// import { Grid } from './functions/class-grid.js';
+import { Grid } from './functions/class-grid.js';
 import { nextGen } from './functions/next-generation.js';
 
-// export const gridSize = 6;
-// let grid = new Grid(gridSize);
+export const gridSize = 6;
+let grid = new Grid(gridSize);
 
-const testGrid1 = {
-    array: [
-        [1, 1, 0, 0, 0],
-        [1, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 1],
-        [0, 0, 0, 1, 1],
-    ],
-};
-
-const grid = nextGen(testGrid1);
+grid = nextGen(testGrid1);
 console.log(grid);

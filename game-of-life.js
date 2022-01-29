274a94d39d4ae4d32632508434bdc70d115b7749
Antonio Morga <anotniomc9719@gmail.com>
2022-01-29 19:36:10 +0100

    added tests for findAliveCells and findDeadCells

--- game-of-life.js
+++ game-of-life.js
@@ -1,8 +1,18 @@
-import { Grid } from './functions/class-grid.js';
+// import { Grid } from './functions/class-grid.js';
 import { nextGen } from './functions/next-generation.js';
 
-export const gridSize = 6;
-let grid = new Grid(gridSize);
+// export const gridSize = 6;
+// let grid = new Grid(gridSize);
 
-grid = nextGen(grid);
+const testGrid1 = {
+    array: [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+    ],
+};
+
+const grid = nextGen(testGrid1);
 console.log(grid);
